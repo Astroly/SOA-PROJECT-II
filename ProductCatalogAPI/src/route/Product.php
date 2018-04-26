2ed6eca7d92b5c55e14ec9baebdd0ba8addb8eb3
NitiponEXP <nitipon000@gmail.com>
2018-04-26 23:32:46 +0700

    edit

--- ProductCatalogAPI/src/route/Product.php
+++ ProductCatalogAPI/src/route/Product.php
@@ -102,7 +102,7 @@ $app->put('/api/product/update/{id}',function(Request $request, Response $respon
         echo '{"error": {"text": '.$e->getMessage().'}' ;
     }
     //Delete Product
-@app->delete('/api/product/delete{id}', function(Request $requset, Response $response){
+    $app->delete('/api/product/delete{id}', function(Request $requset, Response $response){
     $id = $request->getAttribute('id');
     //$sql = "DELETE FROM customer_contact WHERE id=$id";
     try{
