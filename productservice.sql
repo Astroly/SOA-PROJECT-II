2ae7187de4fecb8ed509e7108cedf277b87ecc57
poramet003 <s5830213003@phuket.psu.ac.th>
2018-05-01 22:29:47 +0700

    Arrange

--- productservice.sql
+++ productservice.sql
@@ -18,15 +18,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
 -- Database: `productservice`
---
-
 -- --------------------------------------------------------
-
---
 -- Table structure for table `product`
---
 
 CREATE TABLE `product` (
   `productID` int(5) NOT NULL,
